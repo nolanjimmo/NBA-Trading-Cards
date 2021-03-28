fb095032dfad3ea9ca86fe39b046105ed06bf76a
Dean Stuart <dean.stuart@uvm.edu>
2021-03-28 13:31:53 -0400

    added about page

--- flask_app_.py
+++ flask_app_.py
@@ -5,13 +5,20 @@
 
 from flask import Flask, render_template
 
-app =  Flask(__name__)
+app = Flask(__name__)
 
 button_state = False
+
 
 @app.route("/")
 def home():
     return render_template("main_page.html", b_state=button_state)
+
+
+@app.route("/about")
+def about():
+    return render_template("about_us.html")
+
 
 @app.route("/switch_state/", methods=['POST'])
 def flip_button_state():
@@ -20,7 +27,8 @@ def flip_button_state():
         button_state = False
     else:
         button_state = True
-    return render_template("main_page.html", b_state = button_state)
+    return render_template("main_page.html", b_state=button_state)
+
 
 if __name__ == "__main__":
     app.run()
