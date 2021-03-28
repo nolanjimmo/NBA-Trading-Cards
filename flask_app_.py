391217efa4144879b819c5c53ee8f133b7ad168e
nolanjimmo <barcafan12345@gmail.com>
2021-03-28 11:52:25 -0400

    First setup of flask

---
+++ flask_app_.py
@@ -0,0 +1,26 @@
+##CS205 Final Project 
+##Charles Morgan, Nolan Jimmo, Dean Stuart, George Fafard
+
+##Beginning of the flask app for the interface of the project
+
+from flask import Flask, render_template
+
+app =  Flask(__name__)
+
+button_state = False
+
+@app.route("/")
+def home():
+    return render_template("main_page.html", b_state=button_state)
+
+@app.route("/switch_state/", methods=['POST'])
+def flip_button_state():
+    global button_state
+    if button_state:
+        button_state = False
+    else:
+        button_state = True
+    return render_template("main_page.html", b_state = button_state)
+
+if __name__ == "__main__":
+    app.run()
