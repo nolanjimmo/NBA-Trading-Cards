0ebf68b17ae487d8696a7108f10599f3bb2d957c
Charles Morgan <cjmorgan2000@gmail.com>
2021-04-03 17:48:54 -0400

    Add delete_trade and do_trade functions to interface

--- query_engine.py
+++ query_engine.py
@@ -91,7 +91,7 @@ def create_user(user_data: Tuple[int, str, List[int], List[int]]) -> User:
     return User(user_data[0], user_data[1], tuple(user_data[2]), tuple(user_data[3]))
 
 
-def check_user_has_cards(user_cards: Tuple[int], cards: List[int]):
+def check_user_has_cards(user_cards: Tuple[int], cards: Tuple[int]):
     for card in cards:
         if card not in user_cards:
             return False
@@ -106,6 +106,12 @@ class QueryEngine:
         self.conn = sqlite3.connect(db_loc, detect_types=sqlite3.PARSE_DECLTYPES)
         sqlite3.register_adapter(list, json_list_adapter)
         sqlite3.register_converter("json", json_list_converter)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
 
     def get_card_from_id(self, card_id: int) -> Card:
         output = self.conn.execute("select * from Cards where id = ?", (card_id,)).fetchone()
@@ -153,7 +159,6 @@ class QueryEngine:
 
         return tuple(user_trades)
 
-    # TODO: Write functions to add users and trades
     def add_user(self, username: str, cards: List[int], trades: List[int]) -> None:
         try:
             self.conn.execute("insert into Users (name, cards, trades) values (?, ?, ?)", (username, cards, trades))
@@ -183,14 +188,58 @@ class QueryEngine:
         self.conn.execute("update Users set trades = ? where id = ?", (new_user_trades, user_id))
         self.conn.commit()
 
+    def check_valid_trade(self, user1_id: int, user1_cards: Tuple[int], user2_id: int, user2_cards: Tuple[int]) -> bool:
+        return check_user_has_cards(self.get_user_from_id(user1_id).cards, user1_cards) and \
+                check_user_has_cards(self.get_user_from_id(user2_id).cards, user2_cards)
+
     def create_trade(self, user1_id: int, user1_cards: List[int], user2_id: int, user2_cards: List[int]):
-        if check_user_has_cards(self.get_user_from_id(user1_id).cards, user1_cards) and \
-                check_user_has_cards(self.get_user_from_id(user2_id).cards, user2_cards):
+        if self.check_valid_trade(user1_id, tuple(user1_cards), user2_id, tuple(user2_cards)):
             if not self.__check_trade_exists(user1_id, user1_cards, user2_id, user2_cards):
                 self.__add_trade(user1_id, user1_cards, user2_id, user2_cards)
                 new_trade: Trade = self.get_trade_from_values(user1_id, user1_cards, user2_id, user2_cards)
                 self.__add_trade_to_user(user1_id, new_trade.id)
                 self.__add_trade_to_user(user2_id, new_trade.id)
+
+    def __remove_trade_from_user(self, user_id: int, trade_id: int):
+        user_trades: List[int] = list(self.get_user_from_id(user_id).trades)
+        user_trades.remove(trade_id)
+        self.conn.execute("update Users set trades = ? where id = ?", (user_trades, user_id))
+        self.conn.commit()
+
+    def __delete_trade(self, trade_id: int):
+        self.conn.execute("delete from Trades where id = ?", (trade_id,))
+        self.conn.commit()
+
+    def delete_trade(self, trade_id: int):
+        t: Trade = self.get_trade_from_id(trade_id)
+        self.__remove_trade_from_user(t.user1_id, trade_id)
+        self.__remove_trade_from_user(t.user2_id, trade_id)
+        self.__delete_trade(trade_id)
+
+    def add_card_to_user(self, user_id: int, card_id: int):
+        u: User = self.get_user_from_id(user_id)
+        new_user_cards: List[int] = list(u.cards)
+        new_user_cards.append(card_id)
+        self.conn.execute("update Users set cards = ? where id = ?", (new_user_cards, user_id))
+        self.conn.commit()
+
+    def remove_card_from_user(self, user_id, card_id):
+        user_cards: List[int] = list(self.get_user_from_id(user_id).cards)
+        user_cards.remove(card_id)
+        self.conn.execute("update Users set cards = ? where id = ?", (user_cards, user_id))
+        self.conn.commit()
+
+    def do_trade(self, trade_id: int):
+        t: Trade = self.get_trade_from_id(trade_id)
+        if self.check_valid_trade(t.user1_id, t.user1_cards, t.user2_id, t.user2_cards):
+            self.delete_trade(trade_id)
+            for card in t.user1_cards:
+                self.remove_card_from_user(t.user1_id, card)
+                self.add_card_to_user(t.user2_id, card)
+
+            for card in t.user2_cards:
+                self.remove_card_from_user(t.user2_id, card)
+                self.add_card_to_user(t.user1_id, card)
 
 
 def load_database(db_loc: str, schema_loc: str) -> None:
@@ -213,35 +262,30 @@ def load_database(db_loc: str, schema_loc: str) -> None:
 
                 cursor = conn.cursor()
                 cursor.executemany(sql, cards)
+                cursor.close()
 
         conn.commit()
 
 
-def load_test_data(db_loc: str) -> None:
-    conn: sqlite3.Connection
+def load_test_data(qe: QueryEngine) -> None:
+    qe.add_user("chuck", [1, 2, 3], [])
+    qe.add_user("nolan", [4, 5, 6], [])
+    qe.add_user("dean", [7, 8, 9], [])
+    qe.add_user("george", [10, 11, 12], [])
 
-    with sqlite3.connect(db_loc, detect_types=sqlite3.PARSE_DECLTYPES) as conn:
-        sqlite3.register_adapter(list, json_list_adapter)
-        sqlite3.register_converter("json", json_list_converter)
-        # TODO: Create csv files with test data for Users and Trades to make this easier and more robust
-        conn.execute("insert into Users (name, cards, trades) values ('chuck', ?, ?), ('nolan', ?, ?)",
-                     ([1, 2, 3], [1], [4], [1]))
-        conn.execute("insert into Trades (user1_id, user1_cards, user2_id, user2_cards) values (1, ?, 2, ?)",
-                     ([1], [4]))
-        conn.commit()
+    qe.create_trade(1, [2], 2, [4])
+    qe.create_trade(3, [7, 8], 4, [10])
 
 
 if __name__ == "__main__":
     if os.path.exists(db_filename):
         os.remove(db_filename)
     load_database(db_filename, schema_filename)
-    load_test_data(db_filename)
-    qe = QueryEngine(db_filename)
-    user1 = qe.get_user_from_id(1)
-    user2 = qe.get_user_from_username('chuck')
-    qe.add_user("dean", [5, 56], [])
-    print(qe.get_user_trades(1))
-    print(qe.get_user_trades(2))
-    qe.create_trade(1, [3], 2, [4])
-    print(qe.get_user_trades(1))
-    print(qe.get_user_trades(2))
+
+    with QueryEngine(db_filename) as qe:
+        load_test_data(qe)
+        print(qe.get_user_from_id(1))
+        print(qe.get_user_from_id(2))
+        qe.do_trade(1)
+        print(qe.get_user_from_id(1))
+        print(qe.get_user_from_id(2))
