df80e2ae8536f8dc0f9c0a61a5acd985f38719d7
Dean Stuart <dean.stuart@uvm.edu>
2021-04-04 14:35:47 -0400

    started working on registering new users

--- flask_app_.py
+++ flask_app_.py
@@ -10,7 +10,7 @@ db_filename = "trading_card_data.db"
 schema_filename = "trading_card_schema.sql"
 
 if os.path.exists(db_filename):
-        os.remove(db_filename)
+    os.remove(db_filename)
 load_database(db_filename, schema_filename)
 ###Test data for the database
 qe = QueryEngine(db_filename)
@@ -24,7 +24,7 @@ curr_user = None
 
 @app.route("/")
 def home():
-    return render_template("main_page.html", u_name = username)
+    return render_template("main_page.html", u_name=username)
 
 
 @app.route("/about")
@@ -39,7 +39,7 @@ def sign_in():
     global curr_user
     valid_user = None
     username = request.form['username']
-    #here, we query the database for the username
+    # here, we query the database for the username
     try:
         curr_user = qe.get_user_from_username(username)
         return render_template("successful_sign_in.html", u_name=username, valid_user=valid_user)
@@ -47,28 +47,46 @@ def sign_in():
         valid_user = False
         return render_template("main_page.html", u_name=username, valid_user=valid_user)
 
+
+@app.route("/sign_up", methods=('GET', 'POST'))
+def sign_up():
+    qe = QueryEngine(db_filename)
+    global username
+    global curr_user
+    valid_user = None
+    if request.method == 'POST':
+        username = request.form['username']
+
+        if qe.get_user_from_id(username) is None:
+            qe.add_user(username, [], [])
+
+        curr_user = qe.get_user_from_username(username)
+
+        return render_template("successful_sign_in.html", u_name=username, valid_user=valid_user)
+
+
 @app.route("/selection", methods=['POST'])
 def decision():
     qe = QueryEngine(db_filename)
     choice = request.form['selection']
     if choice == "display":
         card_list = []
-        #Create player classes for each of the player cards that the user has
-        #Store them in a list, then pass that list to the render_template()
+        # Create player classes for each of the player cards that the user has
+        # Store them in a list, then pass that list to the render_template()
         card_list = qe.get_user_cards(curr_user.id)
-        #display the users cards by passing list to display_cards.html
+        # display the users cards by passing list to display_cards.html
         return render_template("display_cards.html", c_list=card_list)
 
     elif choice == "buy":
         all_cards = qe.get_all_cards()
-        #allow the user to look at all cards available
-        return render_template("buy_cards.html", all_cards = all_cards, qe=qe)
+        # allow the user to look at all cards available
+        return render_template("buy_cards.html", all_cards=all_cards, qe=qe)
     else:
-        trade_list=[]
-        #Here, we are going to display the list of trades the user is currently involved in
+        trade_list = []
+        # Here, we are going to display the list of trades the user is currently involved in
         for trade in curr_user.trades:
             trade_list.append(qe.get_trade_from_id(trade))
-        #display the trading interface
+        # display the trading interface
         return render_template("trade_cards.html", t_list=trade_list, qe=qe)
 
 
