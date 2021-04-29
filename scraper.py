60430e760a54ecb9b152ad096faf491cdd498c4e
Dean Stuart <dean.stuart@uvm.edu>
2021-04-29 16:41:48 -0400

    changed from txt to csv file

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import csv
 import requests
 from bs4 import BeautifulSoup
 
@@ -9,17 +10,25 @@ results = soup.find(id='totals_stats')
 
 players = results.find_all('tr', class_='full_table')
 
-f = open("players_pic.txt", "w")
-
+f = open("players_pic.csv", "w")
 
 for p in players:
     link = p.find('a')['href']
+    name = p.find('a')
+    name = str(name)
+    name = name[36:][:-4]
     link = link[:-4] + "jpg"
     prelink = link[0:9]
     postlink = link[11:]
     link = prelink + postlink
-    print(link)
+    # print(name)
+    with open('NBAdata.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csvreader:
+            if (name == row[0]):
+                print(row[0])
+                f.write("https://www.basketball-reference.com/req/202104203/images" + link + "," + name +  "\n")
 
-    f.write("https://www.basketball-reference.com/req/202104203/images" + link + "\n")
+
 
 f.close()
