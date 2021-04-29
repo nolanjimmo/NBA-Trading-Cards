2a2757b169fe19051b741b6ad2de74ca85618c5a
Dean Stuart <dean.stuart@uvm.edu>
2021-04-29 16:58:10 -0400

    ordered NBAdata.csv alphabetically

--- scraper.py
+++ scraper.py
@@ -19,11 +19,12 @@ for p in players:
     name = name[36:][:-4]
     link = link[0:9] + link[11:]
     print(name)
-    with open('NBAdata.csv', newline='') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-        for row in csvreader:
-            if name == row[0]:
-                # print(row[0])
-                f.write(name + "," + "https://www.basketball-reference.com/req/202104203/images" + link + "\n")
+    f.write(name + "," + "https://www.basketball-reference.com/req/202104203/images" + link + "\n")
+    # with open('NBAdata.csv', newline='') as csvfile:
+    #     csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    #     for row in csvreader:
+    #         if name == row[0]:
+    #             # print(row[0])
+    #             f.write(name + "," + "https://www.basketball-reference.com/req/202104203/images" + link + "\n")
 
 f.close()
