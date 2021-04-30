896a3b9a08889b6e6ab2df4ece2bea3110309474
Dean Stuart <dean.stuart@uvm.edu>
2021-04-30 14:03:25 -0400

    replaced all special charatcers in names

--- scraper.py
+++ scraper.py
@@ -16,10 +16,33 @@ for p in players:
     link = p.find('a')['href']
     link = link[:-4] + "jpg"
     name = str(p.find('a'))
-    name = name[36:][:-4]
+    startIndex = name.index(">")
+    name = name[startIndex + 1:][:-4]
     link = link[0:9] + link[11:]
+    name = name.replace('ā', 'a')
+    name = name.replace('á', 'a')
+    name = name.replace('ć', 'c')
+    name = name.replace('č', 'c')
+    name = name.replace('Č', 'C')
+    name = name.replace('İ', 'I')
+    name = name.replace('í', 'i')
+    name = name.replace('ž', 'z')
+    name = name.replace('ņ', 'n')
+    name = name.replace('ģ', 'g')
+    name = name.replace('ū', 'u')
+    name = name.replace('Ž', 'Z')
+    name = name.replace('ó', 'o')
+    name = name.replace('ò', 'o')
+    name = name.replace('ö', 'o')
+    name = name.replace('è', 'e')
+    name = name.replace('é', 'e')
+    name = name.replace('š', 's')
+    name = name.replace('Š', 'S')
+    name = name.replace('ý', 'y')
+
     print(name)
     f.write(name + "," + "https://www.basketball-reference.com/req/202104203/images" + link + "\n")
+
     # with open('NBAdata.csv', newline='') as csvfile:
     #     csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     #     for row in csvreader:
